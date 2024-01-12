ccf40ace413e3dc387334689d797d1f69fe1c1e8
minierparedes <3thancr0wn@gmail.com>
2024-01-12 23:18:56 +0900

    update: basic html structure

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,21 @@
 <template>
-  <header class="header">
-    <div class="header-inner">Header</div>
+  <header class="primary-header">
+    <div class="container">
+      Header
+      <a href="#" target="_blank" rel="noopener noreferrer"
+        ><img src="../assets/images/logo-felix.svg" alt="felix hall"
+      /></a>
+      <nav class="primary-navigation">
+        <ul>
+          <li><a href="#">ホーム</a></li>
+          <li><a href="#">ホールについて</a></li>
+          <li><a href="#">ホール利用</a></li>
+          <li><a href="#">スタジオ利用</a></li>
+          <li><a href="#">音楽教室</a></li>
+          <li><a href="#">コンサート情報</a></li>
+          <li><a href="#">お問い合わせ</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,10 @@
 import { useSeoMeta } from '#imports';
 <template>
-  <div>
+  <div class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
     <h1>Index page</h1>
   </div>
 </template>
+
 <script lang="ts" setup>
   import { useSeoMeta } from '#imports'
 
