fdcc797a8cea758c833b85e929b07fc518006b90
minierparedes <3thancr0wn@gmail.com>
2024-02-06 02:01:55 +0900

    refactor(component): add mobile nav within a threshold

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,23 @@
+<script setup>
+  import { ref, onUnmounted, onMounted } from 'vue'
+
+  const isDesktop = ref(true)
+
+  const handleResize = () => {
+    isDesktop.value = window.innerWidth >= 768
+  }
+
+  onMounted(() => {
+    window.addEventListener('resize', handleResize)
+    handleResize()
+  })
+  onUnmounted(() => {
+    window.removeEventListener('resize', handleResize)
+  })
+</script>
 <template>
   <nav>
-    <TheNavBar />
+    <TheNavBar v-if="isDesktop" />
+    <TheMobileNavBar v-else />
   </nav>
-  <!-- mobile navbar -->
 </template>
